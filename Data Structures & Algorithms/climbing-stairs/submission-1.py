10172e3de61f67e4e1914c5f612a66458d282bf3
Zarin Subah <133192570+Zarin21@users.noreply.github.com>
2026-08-21 13:55:07 -0600

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [1, 2]
+
+        if n < 3:
+            return dp[n-1]
+
+        for i in range(2, n):
+            dp.append(dp[i-1]+dp[i-2])
+        return dp[n-1]
